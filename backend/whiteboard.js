4da133dba8fef87f1af7c19739d62449e3f51716
Ashutosh0602 <ashu00jnp@gmail.com>
2024-07-07 00:34:48 +0530

    fixed some bugs

--- backend/whiteboard.js
+++ backend/whiteboard.js
@@ -14,9 +14,9 @@ const io = new Server(server, {
 // Class for each operation log
 class OperationLog {
   constructor(type, data) {
-    this.type = type; // draw, text, shape
+    this.type = type;
     this.data = data; // { prevX, prevY, currX, currY, tool, color } for draw, { x, y, text, color } for text, { shape, startX, startY, endX, endY, color } for shape
-    this.next = null; // Pointer to next log in linked list
+    this.next = null;
   }
 }
 
@@ -27,7 +27,6 @@ class OperationLogLinkedList {
     this.tail = null;
   }
 
-  // Add a new log to the end of the linked list
   addLog(type, data) {
     const newLog = new OperationLog(type, data);
     if (!this.head) {
@@ -38,8 +37,6 @@ class OperationLogLinkedList {
       this.tail = newLog;
     }
   }
-
-  // Get all logs as an array (for initialization)
   getAllLogs() {
     let current = this.head;
     const logs = [];
@@ -56,22 +53,18 @@ const operationLogs = new OperationLogLinkedList();
 io.on("connection", (socket) => {
   console.log("A user connected");
 
-  // Emit existing operation logs to the newly connected user
   socket.emit("init", operationLogs.getAllLogs());
 
-  // Listen for drawing events
   socket.on("draw", (data) => {
     operationLogs.addLog("draw", data);
     socket.broadcast.emit("draw", data);
   });
 
-  // Listen for text input events
   socket.on("text", (data) => {
     operationLogs.addLog("text", data);
     socket.broadcast.emit("text", data);
   });
 
-  // Listen for shape events
   socket.on("shape", (data) => {
     operationLogs.addLog("shape", data);
     socket.broadcast.emit("shape", data);
